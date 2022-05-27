aad00372d62c9bb9cda72d3280d191103c85ea33
PhySci <mushenokf@gmail.com>
2022-05-27 22:58:21 +0300

    Fix DB connection

--- src/db.py
+++ src/db.py
@@ -1,9 +1,11 @@
 import datetime
 import logging
+import os
 
 from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, DateTime
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 from settings import DATABASE_URL
 
@@ -20,9 +22,7 @@ class User(Base):
     last_name = Column(String)
     patronymic_name = Column(String)
     email = Column(String)
-    # registration_dt = Column(
-    #     DateTime,
-    #     server_default=datetime.datetime.utcnow)
+    registration_dt = Column(DateTime(timezone=True), server_default=func.now())
     receipts = relationship("Receipt")
 
 
@@ -38,12 +38,14 @@ class Receipt(Base):
 class DBDriver:
 
     def __init__(self):
-        self._db_url = DATABASE_URL
+        self._db_url = os.environ.get("DATABASE_URL")
         self._engine = create_engine(self._db_url)
-        self._session = sessionmaker().configure(bind=self._engine)
+        self._sm = sessionmaker(bind=self._engine)
+        #Base.metadata.create_all(self._engine)
 
     def __del__(self):
-        self._session.close()
+        pass
+        #self._session.close()
 
     def add_user(self, user: dict):
         new_user = User(
@@ -53,8 +55,10 @@ class DBDriver:
             patronymic_name=user['patronymic_name'],
             email=user['email'],
         )
-        self._session.add(new_user)
-        self._session.commit()
+        session = self._sm()
+        session.add(new_user)
+        session.commit()
+        session.close()
 
     def add_receipt(self, receipt: dict):
         pass
