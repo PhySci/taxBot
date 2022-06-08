7e383ab44c823134c4f50bc2afd329358fc953e2
PhySci <mushenokf@gmail.com>
2022-06-08 21:13:38 +0300

    Minor fixes of DB methods

--- src/db.py
+++ src/db.py
@@ -70,8 +70,9 @@ class DBDriver:
     def reinit(self):
         session = self._sm()
         try:
-            session.query(User).delete()
             session.query(Receipt).delete()
+            session.query(User).delete()
+            session.commit()
         except Exception as err:
             print(repr(err))
 
@@ -189,9 +190,8 @@ class DBDriver:
                 _logger.exception(error)
             try:
                 element["update_dt"] = element["update_dt"].strftime("%d-%m-%Y")
-            except AttributeError as error:
+            except AttributeError:
                 element["update_dt"] = None
-                _logger.exception(error)
             res["data"].append(element)
         return res
 
